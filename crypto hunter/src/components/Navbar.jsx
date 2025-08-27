16b5e1e711a200dfe60a8c2e5d118fec03158a10
sayanadhi03 <timepasswithsa@gmail.com>
2025-08-27 22:02:13 +0530

    add some features

---
+++ crypto hunter/src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className='bg-amber-600 text-3xl font-bold p-10'>Navbar hello i am navbar</div>
+  )
+}
+
+export default Navbar
